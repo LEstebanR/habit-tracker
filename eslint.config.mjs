576828ce093ab300343c3231b5dfec4d500b160f
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-09-27 15:56:32 -0500

    fix: actions errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,15 +27,7 @@ const eslintConfig = [
       'next-env.d.ts',
     ],
   },
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...compat.config({
-    extends: ['prettier'],
-    plugins: ['prettier'],
-    rules: {
-      'prettier/prettier': 'error',
-      'sort-keys': ['error', 'asc', { caseSensitive: true, natural: false }],
-    },
-  }),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
 ]
 
 export default eslintConfig
