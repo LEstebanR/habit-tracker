39919e94162732a92cf2616e5a469f296e9e37dc
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-09-26 21:51:26 -0500

    fix: google auth

--- auth.ts
+++ auth.ts
@@ -3,7 +3,12 @@ import Google from "next-auth/providers/google";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
   callbacks: {
     async signIn({ user }) {
       try {
