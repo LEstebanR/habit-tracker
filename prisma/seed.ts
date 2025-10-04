4eba03dad53b9584cf39675489568da928118b25
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-10-04 09:19:59 -0500

    fix: deploy

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
+export const userData: Prisma.UserCreateInput[] = [
   {
     createdAt: new Date(),
     email: 'lesteban.dev@gmail.com',
